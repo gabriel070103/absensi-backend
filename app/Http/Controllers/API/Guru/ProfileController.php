8c699e89fd891d49210c7f63cae1411f8f437c5c
gabriel070103 <hewot231s@gmail.com>
2021-05-07 22:51:08 +0700

    edit profile guru

--- app/Http/Controllers/API/Guru/ProfileController.php
+++ app/Http/Controllers/API/Guru/ProfileController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers\API\Guru;
 
-use App\Http\Controllers\Controller;
+// use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Auth;
+// use Auth;
+use Image;
 
 class ProfileController extends Controller
 {
-    //
+    //edit profile guru
+    public function update_avatar(Request $request){
+        if($request->hasFile('avatar')){
+            $avatar = $request->file('avatar');
+            $filename = time() . '.' . $avatar->getClientOriginalExtension();
+            Image::make($avatar)->resize(300, 300)->save( public_path('upload/avatar/' . $filename));
+
+            $user -> avatar = $filename;
+            $user ->save();
+        }
+    }
 }
